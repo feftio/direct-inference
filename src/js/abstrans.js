b48fd4e6c38935b861ec689d75d93490aa9b537f
Lik Eduard <feft99@gmail.com>
2020-10-06 12:09:37 +0600

    Done easy version

--- src/js/abstrans.js
+++ src/js/abstrans.js
@@ -1,3 +1,16 @@
 window.onload = function() {
-    
+    main();
+}
+
+function main() {
+    let cls = document.getElementById("class"); 
+    obj = document.getElementById("object");
+    inputs = document.getElementsByTagName("input");
+
+    for (let radio of inputs) radio.addEventListener("change", (e) => {
+        if (e.target.id == "supports") obj.innerHTML = "Table(Стол)";
+        if (e.target.id == "walls") obj.innerHTML = "Box(Ящик)";
+        if (e.target.id == "partitions") obj.innerHTML = "Rack(Стеллаж)";
+        if (e.target.id == "doors") obj.innerHTML = "Cabinetry(Ящик/Пенал)";
+    });
 }
