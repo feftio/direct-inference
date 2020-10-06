dd6a50fda83bde478f573c1722ca96b539df167a
Lik Eduard <feft99@gmail.com>
2020-10-06 19:05:42 +0600

    Rewritted CSS Design

--- src/js/abstrans.js
+++ src/js/abstrans.js
@@ -8,9 +8,9 @@ function main() {
     inputs = document.getElementsByTagName("input");
 
     for (let radio of inputs) radio.addEventListener("change", (e) => {
-        if (e.target.id == "supports") obj.innerHTML = "Table(Стол)";
-        if (e.target.id == "walls") obj.innerHTML = "Box(Ящик)";
-        if (e.target.id == "partitions") obj.innerHTML = "Rack(Стеллаж)";
-        if (e.target.id == "doors") obj.innerHTML = "Cabinetry(Ящик/Пенал)";
+        if (e.target.id == "supports") obj.innerHTML = "Стол";
+        if (e.target.id == "walls") obj.innerHTML = "Ящик";
+        if (e.target.id == "partitions") obj.innerHTML = "Стеллаж";
+        if (e.target.id == "doors") obj.innerHTML = "Ящик/Пенал";
     });
 }
